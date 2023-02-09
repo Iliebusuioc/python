4353cd3d43c01afff42f6d7331ab3e11813448ea
unknown <2btc05@IESRAMONLLULL.LOCAL>
2023-02-09 13:04:46 +0100

    Ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -1,20 +1,38 @@
-palabra_secreta = "gato"
+palabra_secreta = "patata"
 
 letras_correctas = []
 letras_incorrectas = []
 
-while True:
+seguir_jugando = True
+
+
+
+while seguir_jugando:
+    letra_pedida = input (" Dime una letra: ")
+
+   
+
+    if letra_pedida not in palabra_secreta:
+        letras_incorrectas.append(letra_pedida)
+    else:
+        for letra in palabra_secreta:
+         if letra == letra_pedida:
+              letras_correctas.append(letra_pedida)
+
     for letra in palabra_secreta:
         if letra in letras_correctas:
             print (letra, end="")
         else:
          print ("_ ", end="")
 
-    letra_pedida = input ("Dime una letra: ")
-
+    
     if letra_pedida in palabra_secreta:
         letras_correctas.append(letra_pedida)
     else:
         letras_incorrectas.append(letra_pedida)
-    print("correctas" ,letras_correctas)
-    print("incorrectas " ,letras_incorrectas)
+    print(" correctas" ,letras_correctas)
+    print(" incorrectas " ,letras_incorrectas)
+
+    if set(letras_correctas) == set(palabra_secreta):
+        seguir_jugando = False
+        print("Ganas!")
